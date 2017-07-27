3da9b0e11d143a9a57ff7b615c917400cab3d975
luandino <lucianoandino.ar@gmail.com>
2017-07-27 10:38:17 +0200

    Developing your neighborhood

---
+++ application.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+import flask
+import urllib
+import requests
+import datetime
+import dateutil.parser
+import random
+import string
+import json
+import os
+from flask import Flask, jsonify, request, url_for, render_template, redirect, send_from_directory, session as login_session
+from pymodm import connect
+import bcrypt
+from datetime import datetime, timedelta
+from flask_pymongo import PyMongo
+
+
+from flask_bootstrap import Bootstrap
+
+def create_app():
+  app = Flask(__name__)
+  Bootstrap(app)
+  return app
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+def url_for_other_page(page):
+    args = request.view_args.copy()
+    args['page'] = page
+    return url_for(request.endpoint, **args)
+app.jinja_env.globals['url_for_other_page'] = url_for_other_page
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+
+
+
+@app.route('/')
+def index():
+    #if 'username' in login_session:
+    #    return 'You are logged in as ' + login_session['username']
+    return flask.render_template('index.html')
+
+
+
+
+
+if __name__ == '__main__':
+    app.secret_key = 'fdsfmkrtjkfmdslfjfssgshfklsnvdsklj'
+    app.run()
